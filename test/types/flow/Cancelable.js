f5673bba014c6aaa82bfc287b60fde3930bfb248
Eazymov <eazymov@mail.ru>
2019-02-17 00:18:30 +0500

    add flow types tests

---
+++ test/types/flow/Cancelable.js
@@ -0,0 +1,16 @@
+/* @flow strict */
+import Cancelable from '@';
+
+let promise: Promise<void> = new Cancelable(() => {});
+
+promise = Cancelable.from(new Promise(() => {}));
+promise = Cancelable.from(promise);
+promise = new Promise(() => {});
+
+let cancelable: Cancelable<void> = Cancelable.from(promise);
+
+cancelable.then((res: void) => res);
+
+cancelable.catch((thrown: mixed) => thrown);
+
+cancelable.finally(() => {});
